6c58fbe904096a3ec174e3ddff40f67989817be5
Mikko Pyykkönen <pyykkonenmikko@gmail.com>
2025-01-17 12:29:26 +0200

    Tehtäviä

--- part12/todo-app/todo-frontend/vite.config.js
+++ part12/todo-app/todo-frontend/vite.config.js
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    watch: {
+      usePolling: true
+    },
+    proxy: {
+      '/api': {
+        target: 'http://backend:3000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
